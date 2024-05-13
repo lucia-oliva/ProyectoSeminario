fc03fdfc3170287d6066c1e2660e9e26859e7dc1
Lucia Oliva <lucia222lr@gmail.com>
2024-05-13 04:02:16 -0300

    actualizacion

--- src/components/StepChat.svelte
+++ src/components/StepChat.svelte
@@ -1,6 +1,6 @@
 <script>
     import { Input, Label, Spinner} from 'flowbite-svelte'
-    import {appStatusInfo, setAppStatusError, setAppStatusChatMode} from '../store';
+    import {appStatusInfo, setAppStatusError} from '../store';
     import SvelteMarkdown from 'svelte-markdown';
    
     const {url, pages, id} = $appStatusInfo;
@@ -8,16 +8,6 @@
     let answer = ''
     let loading =false;
     
-    
-    const numOfImagesToShow = Math.min(pages,4)
-    
-    const images = Array.from({ length: numOfImagesToShow}, (_,i) => {
-        const page= i+1
-        return url 
-        .replace('/upload/', `/upload/w_400,h_540,c_fill,pg_${page}/`)
-        .replace('.pdf', '.jpg')
-    
-    })
 
     const handleSubmit = async (event) => {
         event.preventDefault()
@@ -61,31 +51,26 @@
 
 </script>
 
-
-    <div class="grid grid-cols-4 gap-2">
-        {#each images as image}
-            <img class=" border border-purple-500 rounded w-full h-auto aspect-[280/300]" src={image} alt="PDF page" />
-        {/each}
-    </div>
-
+    <h1 class="animate-fade-down animate-once text-purple-500 text-center text-4xl t"> ¡PDF Listo para Chatear! </h1>
+    
     <form class="mt-8"on:submit={handleSubmit}>
-        <Label for="question" class="block mb-2 text-lg  text-black-700">Ingrese una pregunta:</Label>
+        <Label for="question" class="block mb-2 text-lg  text-white">Ingrese una pregunta:</Label>
         <Input id="question" class="border border-purple-500 bg-purple-100 text-purple-900 px-4 py-2 rounded-md focus:outline-none focus:border-purple-700" required placeholder="¿De que trata este documento?"></Input>
     </form>
 
     {#if loading}
         <div class="mt-10 flex justify-center items-center flex-col gap-y-2">
             <Spinner color="purple"/>
-            <p class="opacity-100">Esperando respuesta... </p>
+            <p class=" text-white opacity-100">Esperando respuesta... </p>
         </div>
     {/if}
 
 {#if answer}
 <div class="mt-8 flex justify-center">
     <div class="max-w-lg w-full bg-purple rounded-lg shadow-xl p-6" >
-        <p class="font-short text-black"><strong>Respuesta:</strong></p>
+        <p class="font-short text-white"><strong>Respuesta:</strong></p>
         {#if answer.trim() !==''}
-        <p class="mt-2 text-black whitespace-pre-line overflow-auto"><SvelteMarkdown source={answer}/> </p>
+        <p class="mt-2 text-white whitespace-pre-line overflow-auto"><SvelteMarkdown source={answer}/> </p>
         {/if}
     </div>
 </div>
